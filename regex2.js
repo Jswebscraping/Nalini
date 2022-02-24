9de53b94970c360aea70e9ec6716832a9f4301cb
Jswebscraping <97273713+Jswebscraping@users.noreply.github.com>
2022-02-24 13:48:39 +0530

    Add files via upload

---
+++ regex2.js
@@ -0,0 +1,23 @@
+
+
+const strings =`Shelf Life
+7 Days
+Unit
+1 kg
+Shelf Life
+7 Days
+Manufacturer Details
+9th floor, Wework Building, Outer Ring Road, Near central mall, Bellandur, Bangalore-560103
+Marketed By
+63 Ideas Infolabs Pvt. Ltd
+Customer Care Details
+Customer Care No. 1-800-208-2400
+Customer Care Mail Id: customercare@handsontrade.com
+Customer Care No. 1-800-208-8888
+Customer Care Mail Id: info@grofers.com
+Disclaimer
+Every effort is made to maintain the accuracy of all information. However, actual product packaging and materials may contain more and/or different information. It is recommended not to solely rely on the information presented.
+`
+const patterns=/Man[\w\s\w,\w\-]{1,}03/g;
+const result=strings.match(patterns);
+console.log(result);
---
+++ rejex.js
@@ -0,0 +1,4 @@
+//const string="https://www.chemistwarehouse.co.nz/buy/1159/betadine-sore-throat-ready-to-use-120ml";
+const pattern=/1+59/
+const result=pattern.exec('https://www.chemistwarehouse.co.nz/buy/1159/betadine-sore-throat-ready-to-use-120ml');
+console.log(result);
