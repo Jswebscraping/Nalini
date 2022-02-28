ea54f9e52df32ce86871bf56bd6a2feb62bfb77c
Jswebscraping <97273713+Jswebscraping@users.noreply.github.com>
2022-02-28 10:36:09 +0530

    Add files via upload

---
+++ chemistwarehouse1.js
@@ -0,0 +1,15 @@
+const puppeteer=require('puppeteer');
+const fs=require('fs');
+(async function(){
+    let links=["https://www.chemistwarehouse.co.nz/buy/41302/biotene-dry-mouth-relief-mouthwash-fresh-mint-470ml","https://www.chemistwarehouse.co.nz/buy/87293/l-oreal-serie-expert-nutrifier-shampoo-300ml"]
+    for(let link of links)
+    {
+    const browser=await puppeteer.launch({headless:false});
+    const page=await browser.newPage();
+    await page.goto(link);
+    await page.waitForXPath(`//*[@id="Left-Content"]/div[3]/div[1]/div/table/tbody/tr[2]/td[2]/div[6]/div/button/div[2]`,{timeout:3000})
+    const st=await page.$x(`//*[@id="Left-Content"]/div[3]/div[1]/div/table/tbody/tr[2]/td[2]/div[6]/div/button/div[2]`)
+    const star=await page.evaluate(span=>span.innerText,st[0])
+    console.log(star)
+    }
+})()
